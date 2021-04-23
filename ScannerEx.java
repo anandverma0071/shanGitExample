fdfba8b88058c1f1d017e380cbf26b7d7364296b
anandverma0071 <anandverma0071@gmail.com>
2021-04-23 11:16:17 -0700

    Scanner Ex

---
+++ ScannerEx.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class ScannerEx{
+	static Scanner sc= new Scanner(System.in);
+	public static void main(String a[]){
+		System.out.println("whoch is your branch");
+		String st=sc.next;
+		System.out.println(st);
+	}
+}
